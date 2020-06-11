d256dc28ea8d2755a307d2c7855ca7dfaa0993c1
soiloui <soilouiem@gmail.com>
2020-06-11 12:19:38 +0200

    added gallery & pricing & improved style

--- scripts/main.js
+++ scripts/main.js
@@ -34,20 +34,180 @@ function smoothScroll(target, duration) {
 	requestAnimationFrame(animationScroll);
 }
 
-const arrows_DOM = document.querySelectorAll('.move-wrapper');
-const navLinks_DOM = document.querySelectorAll('.nav__link');
+const elementsScroll_DOM = document.querySelectorAll('.smooth-scroll');
 
-arrows_DOM.forEach((arrow) => {
-	arrow.addEventListener('click', function (e) {
-		const arrowDestiny = e.target.parentElement.nextElementSibling;
-		smoothScroll(arrowDestiny, 800);
+elementsScroll_DOM.forEach((element) => {
+	element.addEventListener('click', function () {
+		const linkDestiny = element.getAttribute('href');
+		const linkElement = document.querySelector(`${linkDestiny}`);
+		smoothScroll(linkElement, 800);
 	});
 });
 
-navLinks_DOM.forEach((link) => {
-	link.addEventListener('click', function (e) {
-		const linkName = link.dataset.name;
-		const linkDestiny = document.querySelector(`#${linkName}`);
-		smoothScroll(linkDestiny, 800);
-	});
+//PRICING
+const pricing_BTN = document.querySelector('.pricing-button');
+const pricing_DOM = document.querySelector('.pricing');
+
+pricing_BTN.addEventListener('click', () => {
+	pricing_DOM.classList.toggle('pricing--active');
 });
+
+// GALLERY
+const gallery_DIV = document.querySelector('.gallery');
+const close_ICO = gallery_DIV.querySelector('.close');
+const moveLeft_ICO = gallery_DIV.querySelector('.left');
+const moveRight_ICO = gallery_DIV.querySelector('.right');
+
+const gallery_BTN = document.querySelector('.gallery-button');
+let galleryItems_DOM = gallery_DIV.querySelectorAll('.gallery__item');
+
+let galleryContetLoaded = false;
+
+hideGallery = () => {
+	gallery_DIV.querySelector('.gallery__item').style.opacity = 0;
+	gallery_DIV.classList.remove('gallery--active');
+	close_ICO.removeEventListener('click', hideGallery);
+	moveLeft_ICO.removeEventListener('click', swipeLeft);
+	moveRight_ICO.removeEventListener('click', swipeRight);
+	gallery_DIV.querySelector('.gallery__item--active').classList.remove('gallery__item--active');
+	galleryItems_DOM[0].classList.add('gallery__item--active');
+	galleryOpenListener();
+};
+showGallery = () => {
+	gallery_BTN.removeEventListener('click', showGallery);
+	galleryActivation();
+};
+loadContentGallery = () => {
+	galleryContetLoaded = true;
+	const frag = document.createDocumentFragment();
+	const totalWomenModels = 7;
+	const totalMenModles = 6;
+	const maxTotal = 7;
+
+	for (let i = 1; i <= maxTotal; i++) {
+		if (i <= totalWomenModels) {
+			const womenElement = document.createElement('div');
+			womenElement.classList.add('gallery__item');
+			if (i == 1) womenElement.classList.add('gallery__item--active');
+			const womenPhoto = document.createElement('img');
+			womenPhoto.classList.add('gallery__image');
+			womenPhoto.dataset.src = `.././resources/images/gallery/woman_${i}_1000w.jpg`;
+			womenPhoto.dataset.srcset = `.././resources/images/gallery/woman_${i}_500w.jpg 500w, .././resources/images/gallery/woman_${i}_1000w.jpg 1000w, .././resources/images/gallery/woman_${i}_2000w.jpg 2000w`;
+			womenPhoto.sizes = '80vw';
+			womenPhoto.alt = `women model ${i}`;
+			womenElement.appendChild(womenPhoto);
+			frag.appendChild(womenElement);
+		}
+
+		if (i <= totalMenModles) {
+			const menElement = document.createElement('div');
+			menElement.classList.add('gallery__item');
+			const menPhoto = document.createElement('img');
+			menPhoto.classList.add('gallery__image');
+			menPhoto.dataset.src = `.././resources/images/gallery/men_${i}_1000w.jpg`;
+			menPhoto.dataset.srcset = `.././resources/images/gallery/men_${i}_500w.jpg 500w, .././resources/images/gallery/men_${i}_1000w.jpg 1000w, .././resources/images/gallery/men_${i}_2000w.jpg 2000w`;
+			menPhoto.sizes = '80vw';
+			menPhoto.alt = `men model ${i}`;
+			menElement.appendChild(menPhoto);
+			frag.appendChild(menElement);
+		}
+	}
+
+	gallery_DIV.firstElementChild.appendChild(frag);
+	galleryItems_DOM = gallery_DIV.querySelectorAll('.gallery__item');
+};
+
+galleryOpenListener = () => {
+	gallery_BTN.addEventListener('click', showGallery);
+};
+galleryCloseListener = () => {
+	close_ICO.addEventListener('click', hideGallery);
+};
+
+galleryActivation = () => {
+	if (galleryContetLoaded == false) loadContentGallery();
+
+	gallery_DIV.classList.add('gallery--active');
+
+	let position = 0;
+	let opacIsAnimating = false;
+	let opacInterval = null;
+
+	opacAnimation = () => {
+		if (opacIsAnimating === true) {
+			clearInterval(opacInterval);
+			opacIsAnimating == false;
+		}
+
+		let opac = 0;
+		opacInterval = setInterval(frame, 23);
+		opacIsAnimating = true;
+
+		function frame() {
+			if (opac >= 1) {
+				opacIsAnimating = false;
+				clearInterval(opacInterval);
+			} else {
+				opac += 0.05;
+				gallery_DIV.querySelector('.gallery__item--active').style.opacity = opac;
+			}
+		}
+	};
+
+	swipe = (direction) => {
+		const acutalItem = gallery_DIV.querySelector('.gallery__item--active');
+		gallery_DIV.querySelector('.gallery__item').style.opacity = 0;
+
+		if (direction == 'right') {
+			if (position < galleryItems_DOM.length - 1) {
+				const itemToShow = acutalItem.nextElementSibling;
+				acutalItem.classList.remove('gallery__item--active');
+				itemToShow.classList.add('gallery__item--active');
+				itemToShow.src = itemToShow.dataset.src;
+				position++;
+				loadImage();
+			}
+		} else if (direction == 'left') {
+			if (position > 0) {
+				const itemToShow = acutalItem.previousElementSibling;
+				acutalItem.classList.remove('gallery__item--active');
+				itemToShow.classList.add('gallery__item--active');
+				position--;
+			}
+		}
+
+		opacAnimation();
+	};
+
+	loadImage = () => {
+		const acutalImage = gallery_DIV.querySelector('.gallery__item--active').firstElementChild;
+		if (!acutalImage.hasAttribute('src')) {
+			acutalImage.src = acutalImage.dataset.src;
+			acutalImage.srcset = acutalImage.dataset.srcset;
+		}
+	};
+
+	loadImage();
+	opacAnimation();
+	galleryCloseListener();
+
+	moveLeft_ICO.addEventListener(
+		'click',
+		(swipeLeft = () => {
+			swipe('left');
+		})
+	);
+	moveRight_ICO.addEventListener(
+		'click',
+		(swipeRight = () => {
+			swipe('right');
+		})
+	);
+};
+
+if (gallery_DIV.classList.contains('gallery--active')) {
+	galleryCloseListener();
+	galleryActivation();
+} else {
+	galleryOpenListener();
+}
